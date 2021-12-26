0d5ee3368986ca326ad160d91c0372baefb94efb
Kum4r_Princ3 <57240372+Kum4r-Princ3@users.noreply.github.com>
2021-12-26 19:35:39 +0530

    Update Install-Burp-Pro.sh

--- Install-Burp-Pro.sh
+++ Install-Burp-Pro.sh
@@ -20,6 +20,7 @@ echo " Installing Java "
 dpkg-query -W -f='${binary:Package}\n' | grep -E -e '^(ia32-)?(sun|oracle)-java' -e '^openjdk-' -e '^icedtea' -e '^(default|gcj)-j(re|dk)' -e '^gcj-(.*)-j(re|dk)' -e '^java-common' | xargs apt-get -y remove
 apt-get -y autoremove
 dpkg -l | grep ^rc | awk '{print($2)}' | xargs apt-get -y purge
+bash -c 'ls -d /home/*/.java' | xargs rm -rf
 
 
 echo " Making Directory "
