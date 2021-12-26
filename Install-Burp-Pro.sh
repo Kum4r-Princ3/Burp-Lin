504a9f5a8663736ead5691897f456d3d03539cfd
Kum4r_Princ3 <57240372+Kum4r-Princ3@users.noreply.github.com>
2021-12-26 19:44:29 +0530

    Update Install-Burp-Pro.sh

--- Install-Burp-Pro.sh
+++ Install-Burp-Pro.sh
@@ -16,14 +16,16 @@ printf "
 <--------------------------------------------------------------------------------------------------------------------->                                                                                                                   
  
 "
-echo " Installing Java "
+echo " Uninstalling Java "
 dpkg-query -W -f='${binary:Package}\n' | grep -E -e '^(ia32-)?(sun|oracle)-java' -e '^openjdk-' -e '^icedtea' -e '^(default|gcj)-j(re|dk)' -e '^gcj-(.*)-j(re|dk)' -e '^java-common' | xargs apt-get -y remove
 apt-get -y autoremove
 dpkg -l | grep ^rc | awk '{print($2)}' | xargs apt-get -y purge
 bash -c 'ls -d /home/*/.java' | xargs rm -rf
 rm -rf /usr/lib/jvm/*
 for g in ControlPanel java java_vm javaws jcontrol jexec keytool mozilla-javaplugin.so orbd pack200 policytool rmid rmiregistry servertool tnameserv unpack200 appletviewer apt extcheck HtmlConverter idlj jar jarsigner javac javadoc javah javap jconsole jdb jhat jinfo jmap jps jrunscript jsadebugd jstack jstat jstatd native2ascii rmic schemagen serialver wsgen wsimport xjc xulrunner-1.9-javaplugin.so; do sudo update-alternatives --remove-all $g; done
-
+echo " Installing Openjdk-11-Jre "
+apt install openjdk-11-jre
+sleep 5
 echo " Making Directory "
 sleep 2
 mkdir /opt/Burp 
