488be85a3ec09e951a98de0415f653e2a6b44e79
Kum4r_Princ3 <57240372+Kum4r-Princ3@users.noreply.github.com>
2021-12-26 19:15:44 +0530

    Update Install-Burp-Pro.sh

--- Install-Burp-Pro.sh
+++ Install-Burp-Pro.sh
@@ -16,21 +16,28 @@ printf "
  
 "
 echo " Making Directory "
+sleep 2
 mkdir /opt/Burp 
 echo "Done"
+sleep 2
 echo " Moving Files "
+sleep 5
 mv burp-pro-loader.jar /opt/Burp/
 mv license-generator.jar /opt/Burp/
 cd ../
 mv burpsuite_pro_v* /opt/Burp/
 echo " Done "
+sleep 2
 echo " Writting Excutable Commands "
+sleep 5
 echo "cd /opt/burp/ && java --illegal-access=warn  -javaagent:burp-pro-loader.jar -noverify -jar burpsuite_pro_v*.jar" > /bin/burp
 chmod +x /bin/burp
 echo "cd /opt/burp/ && java -jar license-generator.jar " > /bin/ligen
 chmod +x /bin/ligen
 echo " Done "
+sleep 2
 echo " Launching Burp Suite Pro & License Generator "
+sleep 5
 burp | ligen 
 
 
